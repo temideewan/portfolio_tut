0109de8987bbf2c841f1e702a2aeaa8f6a8a48b7
temidayo omoyajowo <temideewan@gmail.com>
2020-12-29 07:50:48 +0100

    feature: complete posts and projects pages

---
+++ portfoliotutoiral/schemas/project.js
@@ -0,0 +1,55 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+
+      type: "string",
+    },
+    {
+      name: "date",
+
+      type: "datetime",
+    },
+    {
+      name: "place",
+
+      type: "string",
+    },
+    {
+      name: "description",
+
+      type: "text",
+    },
+    {
+      name: "projectType",
+      title: "Project type",
+
+      type: "string",
+      options: {
+        list: [
+            { value: "personal", title: "personal" },
+            { value: "client", title: "client" },
+            { value: "school", title: "school" }
+        ],
+      },
+    },
+    {
+        name:'link',
+        type:'url'
+    },
+    {
+        name:'tags',
+        type:'array',
+        of:[
+            {type:'string'}
+        ],
+        options: {
+            layout:'tags'
+        }
+    }
+  ],
+};
